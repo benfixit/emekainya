750a70b5cfcd2de0ac88d582d32f8adff3776d89
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2025-11-02 16:35:05 +0100

    chore: add resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
+import rehypeRaw from 'rehype-raw'
+
+import "./Resume.css";
+
 const Resume = () => {
+    const [content, setContent] = useState("");
+    useEffect(() => {
+        const fetchMarkdownFile = async () => {
+            const resumeFile = await import("../content/resume/index.md");
+            const response = await fetch(resumeFile.default);
+            
+            const data = await response.text();
+            
+            setContent(data);
+        }
+
+        fetchMarkdownFile();
+    }, []);
+
     return (
-        <>
-            <h1>Welcome to the Resume Page</h1>
-        </>
+        <div className="wrapper">
+            <Markdown rehypePlugins={[rehypeRaw]}>{content}</Markdown>
+        </div>
     );
 }
 
