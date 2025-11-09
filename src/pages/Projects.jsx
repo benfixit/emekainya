901bc25bb5ce2149b68b071bd639e2de6c719672
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2025-11-09 09:13:38 +0100

    move projects to json file

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,11 @@
+import data from "../utils/projects.json";
 import "./Projects.css";
 
-const items = [
-    {
-        id: 1,
-        title: "Markdown Editor",
-        description: "Markdown editor with preview using React JS",
-        link: "https://github.com/benfixit/markdown-editor"
-    },
-    {
-        id: 2,
-        title: "Chat app",
-        description: "A chat app built using React, Express and Socket.io",
-        link: "https://github.com/benfixit/chat-app"
-    },
-    {
-        id: 3,
-        title: "Igbo names",
-        description: "A repository for Igbo names",
-        link: "https://github.com/benfixit/igbonames"
-    }
-];
-const Projects = () => {
+
+const Projects = () => {    
     return (
         <section className="projects">
-            {items.map((item, index) => (
+            {data.projects.map((item, index) => (
                 <div key={index}>
                     <a href={`${item.link}`} target="_blank">{item.title}</a>
                     <p>{item.description}...</p>
